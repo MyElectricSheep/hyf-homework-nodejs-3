4599785c9edadc5a341a8b014be72b580a9c9ca9
Ben Paget <ben.paget@live.fr>
2021-02-27 18:00:45 +0000

    step-4

--- src/index.js
+++ src/index.js
@@ -21,6 +21,17 @@ app.get('/user/:id', (req, res) => {
     res.status(200).json({ id: Number(req.params.id) })
 })
 
+app.delete('/user/:id', (req, res) => {
+    const targetUser = users.find(u => u.id === Number(req.params.id))
+    if (!targetUser) {
+        return res.sendStatus(204)
+    } else {
+        const index = users.indexOf(targetUser)
+        users.splice(index, 1);
+        res.status(202).json(targetUser)
+    }
+})
+
 app.listen(3000, () => {
     console.log('listening to port 3000...')
 })
