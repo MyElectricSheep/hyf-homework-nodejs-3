890290460474fe9e545274fada7fd0f2a81a9eaa
Ben Paget <ben.paget@live.fr>
2021-02-27 17:38:24 +0000

    step-3

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ const express = require('express')
 
 const app = express()
 
+const users = []
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.get('/users', (req, res) => {
-    res.send([])
+    res.send(users)
+})
+
+app.post('/user', (req, res) => {
+    users.push({ id: 0 })
+    res.status(200).json({ id: 0 })
+})
+
+app.get('/user/:id', (req, res) => {
+    res.status(200).json({ id: Number(req.params.id) })
 })
 
 app.listen(3000, () => {
